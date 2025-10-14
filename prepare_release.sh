99dec52378c3c44d054ea0a301da2bdd188b81ea
isaiahfuller <9921699+isaiahfuller@users.noreply.github.com>
2025-10-14 04:18:37 -0400

    Add tracker files in bash script (#7)

--- prepare_release.sh
+++ prepare_release.sh
@@ -2,6 +2,12 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 cd $SCRIPT_DIR
+mkdir -p ./rac3/images/maps
+mkdir -p ./rac3/maps
+mkdir -p ./rac3/locations
+cp -r ./rac3-ap-poptracker/maps ./rac3/
+cp -r ./rac3-ap-poptracker/images/maps ./rac3/images/
+cp -r ./rac3-ap-poptracker/locations ./rac3/
 zip -qo -r ./rac3.apworld ./rac3
 
 OUTPUT=rac3jp_ap
@@ -12,3 +18,6 @@ zip -r ./rac3-ap-poptracker{.zip,}
 mv rac3-ap-poptracker.zip $OUTPUT
 zip -r $OUTPUT{.zip,}
 
+rm -rf ./rac3/maps
+rm -rf ./rac3/images/
+rm -rf ./rac3/locations
